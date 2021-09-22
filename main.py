f5b10bcd8620290edf90819a8d640247c770aee7
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2021-09-22 14:26:16 +0700

    comment of main.py

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ Ocki Bagus Pratama © 2020
 
 from utils import CheckPassword
 
+"""
+1. random password generator:   randomPasswordGenerator()
+2. md5:                         md5()
+3. sha1:                        sha1()
+4. sha224:                      sha224()
+5. sha256:                      sha256()
+6. sha384:                      sha384()
+7. sha512:                      sha512() 
+"""
+
 if __name__ == "__main__":
     main = CheckPassword()
     main.print_info()
@@ -21,17 +31,17 @@ if __name__ == "__main__":
                     print("Password: ")
                 else:
                     pas = input("Password: ")
-            elif ops != 0:
+            elif ops != 0: # wrong: -1, 100, and others
                 print("Wrong!\n")
                 continue
-        except ValueError:
+        except ValueError: # wrong: 'asfff' and others
             print("Wrong!\n")
             continue
 
-        if ops == 0:
+        if ops == 0: # call print_info_end()
             main.run(ops)
             break
-        elif ops == 1:
+        elif ops == 1: # randomPasswordGenerator()
             m = main.run(ops)
             print("%s -> %s" % (m[1], m[0]))
             continue
